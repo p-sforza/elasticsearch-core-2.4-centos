dc26610e3dcb0889ea1c01b1184e4245c793a300
psforza@redhat.com <psforza@redhat.com>
2017-10-24 14:23:15 +0200

    <comment>

--- run.sh
+++ run.sh
@@ -4,31 +4,41 @@
 ELASTIC_LOG=/usr/share/elasticsearch/logs/elasticsearch.log
 runuser -l elasticsearch -c 'echo "" > $ELASTIC_LOG';
 
-ELASTIC_PID=$(ps -aux | grep -m1 elastic | grep -v grep | awk '{ print $2 }');
+ELASTIC_PID=$(ps -aux | grep elastic | grep java | grep -v grep | awk '{ print $2 }');
 if [ $ELASTIC_PID ]; then
 	echo "ERROR: ELASTIC IS ALREADY UP!" 
 	exit 1;
+else
+	# START ELASTIC
+        echo "STARTING ELASTIC!";
+        /usr/share/elasticsearch/bin/elasticsearch -d;
+	STARTED=$(grep started $ELASTIC_LOG);
+        while [ "$STARTED" == "" ]; do
+                echo '   elastic not up...';
+                sleep 1;
+                STARTED=$(grep started $ELASTIC_LOG);
+        done
+	echo "ELASTIC IS UP!" 
+	echo "ELASTIC PROCESS: " && ps -aux | grep elastic | grep java | grep -v grep | awk '{ print $2 }' ;
+	echo "LOGFILE:";
+           cat ${ELASTIC_LOG};
+	echo "END OF LOGFILE";
+
+	# STOP ELASTIC
+        echo "SHUTTING DOWN ELASTIC!" && echo "SHUTTING DOWN ELASTIC!" >> "${ELASTIC_LOG}" ;
+
+        ps -aux
+        ELASTIC_PID=$(ps -aux | grep elastic | grep java | grep -v grep | awk '{ print $2 }')
+        kill ${ELASTIC_PID};
+
+        STOPPED=$(grep " closed" ${ELASTIC_LOG});
+        while [ "$STOPPED" == ""  ]; do
+                echo '   elastic not down...';
+                sleep 1;
+                STOPPED=$(grep closed ${ELASTIC_LOG});
+        done
+        echo "ELASTIC IS DOWN!" 
 fi
 
-echo "STARTING ELASTIC!";
-runuser -l elasticsearch -c '/usr/share/elasticsearch/bin/elasticsearch -d';
-
-until [ STARTED=$(grep started $ELASTIC_LOG) ]; do
-	echo '   elastic not up...';
-	sleep 1;
-done
-
-echo "ELASTIC IS UP!" 
-echo "ELASTIC PROCESS: " && ps -aux | grep -m1 elastic;
-
-echo "SHUTTING DOWN ELASTIC";
-ELASTIC_PID=$(ps -aux | grep -m1 elastic | grep -v grep | awk '{ print $2 }')
-kill $ELASTIC_PID;
-
-until [ STOPPED=$(grep closed $ELASTIC_LOG) ]; do
-        echo '   elastic not down...';
-        sleep 1;
-done
-echo "ELASTIC IS DOWN!" 
 echo "SCRIPT END... BYE BYE" 
 
